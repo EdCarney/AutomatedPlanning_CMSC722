cedc3127e5962c7017d648a4b0d65fab35362d2e
Ed Carney <carneyedwardj@gmail,com>
2022-03-19 15:56:32 -0400

    Adds initial empty state definition for satellite problem.

--- GTPyhop/Examples/satellites_htn/main.py
+++ GTPyhop/Examples/satellites_htn/main.py
@@ -9,3 +9,15 @@ domain = gtpyhop.Domain(domain_name)
 # states and rigid relations
 
 state0 = gtpyhop.State("s0")
+state0.calibrated = {}
+state0.cal_target = {}
+state0.power_on = {}
+state0.power_avail = {}
+state0.fuel = {}
+state0.fuel_used = 0
+state0.data_capacity = {}
+state0.data_stored = 0
+state0.have_image = {}
+state0.pointing = {}
+state0.on_board = {}
+state0.supports = {}
