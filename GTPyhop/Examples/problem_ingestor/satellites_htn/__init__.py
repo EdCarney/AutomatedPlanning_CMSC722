1fc4d46786841673bc00b404d52c0146d4874215
Ed Carney <carneyedwardj@gmail,com>
2022-04-03 15:13:09 -0400

    Removes old GTPyhop folder

--- GTPyhop/Examples/problem_ingestor/satellites_htn/__init__.py
+++
@@ -1,7 +0,0 @@
-# kludge to make gtpyhop available regardless of whether the current directory
-# is the Examples directory or its parent (where gtpyhop.py is located)
-#
-import sys
-
-sys.path.append("../")
-import gtpyhop
